60a5ea60cfb4013c4473ec0dab42d29003db2295
Deepali Satpute <165001795+DeepaliSatpute@users.noreply.github.com>
2025-01-21 14:16:32 +0530

    Update upload.js

--- server/Routes/upload.js
+++ server/Routes/upload.js
@@ -1,33 +1,3 @@
-/*const express = require("express");
-const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary");
-
-const router = express.Router();
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "uploads",
-  },
-});
-
-const upload = multer({ storage });
-
-router.post("/upload", upload.single("image"), (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ message: "No file uploaded" });
-    }
-    res.status(200).json({ url: req.file.path });
-  } catch (error) {
-    console.error("Error uploading file:", error);
-    res.status(500).json({ message: "Internal server error", error });
-  }
-});
-
-module.exports = router;*/
-
 const express = require("express");
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
